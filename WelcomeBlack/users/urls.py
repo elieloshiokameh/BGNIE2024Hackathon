144ef3d53c62c8ad19b39365faed32210c60b2d8
Eliel Oshiokameh <elieloshiokameh@Eliels-MacBook-Pro.local>
2024-10-13 11:57:14 +0100

    Landing page, Login paeg

---
+++ WelcomeBlack/users/urls.py
@@ -0,0 +1,13 @@
+# users/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),  # Homepage route
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
+    path('logout/', views.logout_view, name='logout'),
+    path('welcome/', views.welcome_view, name='welcome'),  # Welcome page
+
+]
---
+++ WelcomeBlack/users/views.py
@@ -0,0 +1,38 @@
+# users/views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+
+def welcome_view(request):
+    return render(request, 'home.html')
+
+def home_view(request):
+    return render(request, 'landing.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after signup
+            return redirect('welcome')  # Redirect to welcome page
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)  # Log the user in
+            return redirect('welcome')  # Redirect to welcome page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
